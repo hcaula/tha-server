a51f55f4c03c80957b12d1139e2233be4e0dd100
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-08 16:17:20 -0300

    chore: configure prettier

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope!')
+})
 
 app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
-});
+    return console.log(`server is listening on ${port}`)
+})
