0bb36b4c921394a4d9250645fe69c271e055a9bd
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-09 00:22:56 -0300

    refac(httpStatus): change constants to enums

--- src/common/httpStatus.ts
+++ src/common/httpStatus.ts
@@ -1,9 +1,9 @@
-export const httpStatusCodes = {
-  SUCCESS: 200,
-  BAD_REQUEST: 400,
-  INTERNAL_SERVER_ERROR: 500
+export enum httpStatusCodes {
+  SUCCESS = 200,
+  BAD_REQUEST = 400,
+  INTERNAL_SERVER_ERROR = 500
 }
 
-export const httpErrorMessages = {
-  INTERNAL_SERVER_ERROR: 'Something went wrong. Try again later.'
+export enum httpErrorMessages {
+  INTERNAL_SERVER_ERROR = 'Something went wrong. Try again later.'
 }
