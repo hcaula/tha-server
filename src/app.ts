deb6cbb5ff1639a6120e606d296e2b499e7776ac
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-10 00:36:31 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as http from 'http'
+import cors from 'cors'
 
 import { configureRoutes } from './common/routes'
 import camelizeQueryParamsMiddleware from './middlewares/camelizeQueryParams.middleware'
@@ -9,6 +10,7 @@ const app: express.Application = express()
 const server: http.Server = http.createServer(app)
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(camelizeQueryParamsMiddleware)
 configureRoutes(app)
